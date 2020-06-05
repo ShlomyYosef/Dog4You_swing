70ffd7231a09a5e4573db9ea0b5383f71d4ec338
DT <dvirto@mta.ac.il>
2020-06-05 11:54:06 +0300

    update ui

--- Dog4y/src/View/Login.java
+++ Dog4y/src/View/Login.java
@@ -11,6 +11,7 @@ import javax.swing.JTextField;
 import javax.swing.JFormattedTextField;
 import javax.swing.DropMode;
 import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
@@ -47,29 +48,38 @@ public class Login extends JFrame {
 		
 		passwordField = new JPasswordField();
 		passwordField.setToolTipText("");
-		passwordField.setBounds(198, 277, 139, 20);
+		passwordField.setBounds(171, 250, 139, 20);
 		contentPane.add(passwordField);
 		
 		textField = new JTextField();
 		textField.setToolTipText("Username");
-		textField.setBounds(198, 219, 139, 20);
+		textField.setBounds(171, 208, 139, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JTextPane txtpnLogInTo = new JTextPane();
 		txtpnLogInTo.setEditable(false);
 		txtpnLogInTo.setText("User");
-		txtpnLogInTo.setBounds(131, 219, 57, 20);
+		txtpnLogInTo.setBounds(84, 208, 57, 20);
 		contentPane.add(txtpnLogInTo);
 		
 		JTextPane txtpnPassword = new JTextPane();
 		txtpnPassword.setText("Password");
-		txtpnPassword.setBounds(122, 277, 66, 20);
+		txtpnPassword.setBounds(84, 252, 66, 20);
 		contentPane.add(txtpnPassword);
 		
 		JTextPane txtpnLogInTo_1 = new JTextPane();
+		txtpnLogInTo_1.setEditable(false);
 		txtpnLogInTo_1.setText("Log in to your account");
 		txtpnLogInTo_1.setBounds(131, 177, 139, 20);
 		contentPane.add(txtpnLogInTo_1);
+		
+		JButton btnNewButton = new JButton("Log in");
+		btnNewButton.setBounds(84, 356, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Sign in");
+		btnNewButton_1.setBounds(221, 356, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
 }
--- Dog4y/src/View/UserUI.java
+++ Dog4y/src/View/UserUI.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
 
 public class UserUI extends JFrame {
 
@@ -32,11 +33,34 @@ public class UserUI extends JFrame {
 	 */
 	public UserUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 851, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextPane txtpnDear = new JTextPane();
+		txtpnDear.setEditable(false);
+		txtpnDear.setEnabled(false);
+		txtpnDear.setText("Dear");
+		txtpnDear.setBounds(10, 11, 66, 20);
+		contentPane.add(txtpnDear);
+		
+		JTextPane txtpnSearch = new JTextPane();
+		txtpnSearch.setText("Search");
+		txtpnSearch.setEditable(false);
+		txtpnSearch.setBounds(10, 105, 102, 20);
+		contentPane.add(txtpnSearch);
+		
+		JTextPane txtpnRace = new JTextPane();
+		txtpnRace.setText("Race :");
+		txtpnRace.setBounds(10, 147, 66, 20);
+		contentPane.add(txtpnRace);
+		
+		JTextPane txtpnAge = new JTextPane();
+		txtpnAge.setText("Age:");
+		txtpnAge.setBounds(10, 193, 66, 20);
+		contentPane.add(txtpnAge);
 	}
 
 }
