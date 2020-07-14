8db35435ad055884a4b8b7536ba79b26fa364224
DT <dvirto@mta.ac.il>
2020-07-14 12:38:13 +0300

    create kennel controller

---
+++ Dog4y/src/Controller/KennelController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import DataBase.DogRepository;
+import View.KennelUI;
+
+public class KennelController {
+
+	
+	
+	private KennelUI theView;
+	private DogRepository theModel;
+	
+	public KennelController(KennelUI theView,DogRepository theModel) {
+		
+		this.theModel = theModel;
+		this.theView = theView;
+		
+	this.theView.setVisible(true);	
+	
+	}
+}
