5377ae4abd63a86077734b95060cf42cd81c72e4
DT <dvirto@mta.ac.il>
2020-07-15 16:30:53 +0300

    merge

--- Dog4y/src/model/SingletonDogDetails.java
+++ Dog4y/src/model/SingletonDogDetails.java
@@ -1,15 +1,15 @@
 package model;
-
+//using singletone pattern
 public class SingletonDogDetails {
 
 	private static SingletonDogDetails firstInstance=null;
+	
 	private String[] Age = {"","Puppy", "Young","Adult","Senior"};
 	private String[] Personality = {"","Confident", "Shy/Timid","Independent","Friendly","Adaptable"};
 	private String[] Location = {"","South", "Center","North"};
 	private String[] Breed = {"","Beagle", "Bulldog","Pit Bull","Belgian Tervuren","Boxer","German Shepherd","Golden Retriever","Jack Russell Terrier","Labrador Retriever","Pug","Rottweiler"};
 	private String[] FinalSize = {"","Small","Medium","Large"};
 	private String[] Gender = {"","male", "female"};
-	
 	
 	private  SingletonDogDetails() {}
 	
@@ -19,10 +19,6 @@ public class SingletonDogDetails {
 		}
 		return firstInstance;
 	}
-
-
-	
-	
 	public String[] getAge() {
 		return Age;
 	}
